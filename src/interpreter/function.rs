5a1125fe0a225a1df3baf2501800b080a94253e3
Noah Weninger <nweninge@ualberta.ca>
2014-11-09 23:10:32 -0700

    Add CondFunction and make the tests pass.

--- src/interpreter/function.rs
+++ src/interpreter/function.rs
@@ -1,9 +1,11 @@
 use super::expr::Expression;
-use super::{CompileError};
+use super::{CompileError, is_truthy};
 use super::scope::Scope;
 
 #[cfg(test)]
 use super::lexer;
+#[cfg(test)]
+use super::{TRUE, FALSE};
 
 // Something that can be called with arguments given in the scope
 pub trait Function {
@@ -76,6 +78,29 @@ impl<'a> Function for ExprFunction<'a> {
 	}
 }
 
+// Returns the result of calling the function if the condition is truthy, else 0
+struct CondFunction<'a> {
+	cond: Box<Function + 'a>,
+	func: Box<Function + 'a>,
+}
+impl<'a> CondFunction<'a> {
+	fn new(cond: Box<Function + 'a>, func: Box<Function + 'a>) -> CondFunction<'a> {
+		CondFunction {
+			cond: cond,
+			func: func,
+		}
+	}
+}
+impl<'a> Function for CondFunction<'a> {
+	fn call(&self, scope: &Scope) -> Result<f32, CompileError> {
+		if is_truthy(try!(self.cond.call(scope))) {
+			self.func.call(scope)
+		} else {
+			Ok(0_f32)
+		}
+	}
+}
+
 #[test]
 fn const_function_test() {
 	let f = ConstFunction::new(42_f32);
@@ -85,7 +110,7 @@ fn const_function_test() {
 #[test]
 fn expr_function_test() {
 	let mut s = Scope::new();
-	s.define("a", 9_f32);
+	s.define_var("a", 9_f32);
 	let e = Expression::new(lexer::tokenize("5*3+a").unwrap().as_slice(), &s).unwrap();
 	let f = ExprFunction::new(&e);
 	assert!(f.call(&s).unwrap() == 24_f32);
@@ -95,7 +120,19 @@ fn expr_function_test() {
 fn sin_test() {
 	let a = 2_f32;
 	let mut s = Scope::new();
-	s.define("a", a);
+	s.define_var("a", a);
 	let f = SinFunction::new();
 	assert!(f.call(&s).unwrap() == a.sin());
+}
+
+#[test]
+fn cond_test() {
+	let cond_truthy = ConstFunction::new(TRUE);
+	let cond_falsey = ConstFunction::new(FALSE);
+	let expr = ConstFunction::new(42_f32);
+	let f_truthy = CondFunction::new(box cond_truthy, box expr);
+	let f_falsey = CondFunction::new(box cond_falsey, box expr);
+	let s = Scope::new();
+	assert!(f_truthy.call(&s).unwrap() == 42_f32);
+	assert!(f_falsey.call(&s).unwrap() == 0_f32);
 }
