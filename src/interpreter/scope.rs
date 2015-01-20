954b37c9bcc54b3fcd12ce5d24090d5e80b10ae8
Noah Weninger <nweninge@ualberta.ca>
2015-01-19 20:21:53 -0700

    Give scope more of a type system

--- src/interpreter/scope.rs
+++ src/interpreter/scope.rs
@@ -3,37 +3,48 @@ use super::identifier::Identifier;
 use std::collections::VecMap;
 use std::borrow::Cow;
 
+#[derive(Clone)]
+enum Value<'a> {
+	Number(f32),
+	Function(&'a (Function + 'static)),
+}
+
 pub type CowScope<'a> = Cow<'a, Scope<'a>, Scope<'a>>;
 
 /// Holds a number of variables, functions and their values.
 #[derive(Clone)]
 pub struct Scope<'a> {
-	vars: VecMap<f32>,
-	funcs: VecMap<&'a (Function + 'static)>,
+	values: VecMap<Value<'a>>,
 }
 
+// TODO warn on type errors
 impl<'a> Scope<'a> {
 	/// Constructs an empty scope object.
 	pub fn new() -> Scope<'a> {
 		Scope {
-			vars: VecMap::new(),
-			funcs: VecMap::new(),
+			values: VecMap::new(),
 		}
 	}
 
 	pub fn set_var(&mut self, id: Identifier, value: f32) {
-		self.vars.insert(id, value);
+		self.values.insert(id, Value::Number(value));
 	}
 
 	pub fn get_var(&self, id: Identifier) -> Option<f32> {
-		self.vars.get(&id).map(|x| *x)
+		match self.values.get(&id) {
+			Some(&Value::Number(x)) => Some(x),
+			_ => None,
+		}
 	}
 
 	pub fn set_func(&mut self, id: Identifier, func: &'a (Function + 'static)) {
-		self.funcs.insert(id, func);
+		self.values.insert(id, Value::Function(func));
 	}
 
 	pub fn get_func(&self, id: Identifier) -> Option<&'a (Function + 'static)> {
-		self.funcs.get(&id).map(|x| *x)
+		match self.values.get(&id) {
+			Some(&Value::Function(x)) => Some(x),
+			_ => None,
+		}
 	}
 }
